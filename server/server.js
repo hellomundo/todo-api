95f111c01e767f933e0be526cf335b96a40d6674
hellomundo <ed.ortega@gmail.com>
2017-10-11 10:37:45 -0700

    added first rest endpoint

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {mongoose} = require('./db/mongoose.js')
+const {Todo} = require('./models/todo.js')
+const {User} = require('./models/user.js')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        console.log('error: ', err)
+        res.status(400).send(err)
+    })
+    
+})
+
+app.listen(2000, () => {
+    console.log('Started server on port 2000');
+    
+})
