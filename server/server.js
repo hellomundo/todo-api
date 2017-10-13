a766c6b9804f5f0659b58468531893a72b19280c
hellomundo <ed.ortega@gmail.com>
2017-10-12 22:34:16 -0700

    added DELETE /todos route

--- server/server.js
+++ server/server.js
@@ -52,6 +52,25 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).send()
+    }
+
+    Todo.findByIdAndRemove({_id: id}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send()
+        }
+        
+        res.status(200).send({todo})
+    }).catch((err) => {
+        res.status(400).send(err)
+    })
+
+})
+
 app.listen(port, () => {
     console.log(`Started server on port ${port}`);
     
